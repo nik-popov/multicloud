1d0f274c7e6519a20d9ad94f02e68f0470211233
Nik Popov <apis.popov@gmail.com>
2025-09-27 22:48:02 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCZQI-bU0XrCsmHT1XGNnbYWw7-Jw6D

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth }from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: "studio-1789539808-2f8a9",
   apiKey: "AIzaSyCZQI-bU0XrCsmHT1XGNnbYWw7-Jw6DIpU",
   authDomain: "studio-1789539808-2f8a9.firebaseapp.com",
+  projectId: "studio-1789539808-2f8a9",
+  storageBucket: "studio-1789539808-2f8a9.firebasestorage.app",
+  messagingSenderId: "907565582067",
+  appId: "1:907565582067:web:73e99ab1a1fee88419d986"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
